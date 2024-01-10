c24103a730ec2fa0d2f2fcf241b3c1032aca536a
jovenbarola <jovenbarola@gmail.com>
2024-01-10 11:10:58 +0800

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import './style.css';
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Point Of Sale</h1>
+      <p className='comment'>Deployed in vercel</p>
+      <input type="text" placeholder="Username" /><br />
+      <input type="password" placeholder="Password" /><br />
+      <button>Login</button>
     </div>
   );
 };
