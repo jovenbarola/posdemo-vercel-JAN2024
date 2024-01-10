efc56773838f4f677c43ab052d6ad9f988fcba0f
jovenbarola <jovenbarola@gmail.com>
2024-01-10 11:36:35 +0800

    change styling to NES

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 
-import './style.css';
+import "../node_modules/nes.css/css/nes.css"
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
-    <div>
-      <h1>Point Of Sale</h1>
-      <p className='comment'>Deployed in vercel</p>
-      <input type="text" placeholder="Username" /><br />
-      <input type="password" placeholder="Password" /><br />
-      <button>Login</button>
-    </div>
+    <section className='showcase'>
+      <section className='nes-container with-title'>
+        <h3 className="title">Login</h3>
+        <div className="nes-field">
+          <input className='nes-input is-success' type="text" placeholder="Username" /><br />
+        </div>
+        <div className="nes-field">
+          <input className='nes-input' type="password" placeholder="Password" /><br />
+        </div>
+        <button className='nes-btn is-primary'>Login</button>
+      </section>
+    </section>
   );
 };
